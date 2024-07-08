57139af8bc8129dd51d51eec235c65d38efcb3db
david <davda-si@student.42lisboa.com>
2024-07-08 13:26:38 +0100

    more stuff parser

--- src/parser.c
+++ src/parser.c
@@ -1,35 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/01 15:14:17 by david             #+#    #+#             */
-/*   Updated: 2024/07/07 02:01:46 by david            ###   ########.fr       */
+/*   Created: 2024/07/07 01:55:48 by david             #+#    #+#             */
+/*   Updated: 2024/07/07 02:10:46 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub.h"
 
-int main(int ac, char **av)
+void	check_file(char *file, t_data *game)
 {
-	t_data	game;
+	int	fd;
 
-	if (ac != 2)
+	if ((fd = open(file, __O_DIRECTORY)) != -1)
 		error_handle(0);
-	if (ft_strnstr(av[1] + ft_strlen(av[1]) - 4,
-		".cub", ft_strlen(av[1])) == NULL)
-		error_handle(1);
-	ft_memset(&game, 0, sizeof(t_data));
-	check_file(av[1], &game);
-	return (0);
-	if (ac == 2)
-	{
-		//parsing of the file, then of the map
-		calculus(/*map*/NULL);
-		//run game with all the calculus
-	}
-	else
-		ft_printf("Wrong number of arguments, try again\n");
+	if ((fd = open(file, O_RDONLY)) == -1)
+		error_handle(0);
+	
 }
