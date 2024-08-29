db86dfe442b6cb603dc22f75a5c413ad15b50fa1
Phantasiae <phantasiae.flr@gmail.com>
2024-08-29 19:53:18 +0100

    wowww tanto codigo

--- src/mlx_diy.c
+++ src/mlx_diy.c
@@ -6,13 +6,11 @@
 /*   By: phanta <phanta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 11:30:30 by phanta            #+#    #+#             */
-/*   Updated: 2024/08/29 18:20:47 by phanta           ###   ########.fr       */
+/*   Updated: 2024/08/29 19:53:07 by phanta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub.h"
-
-
 
 void	my_mlx_pixel_put(t_img *data, int x, int y, int color)
 {
