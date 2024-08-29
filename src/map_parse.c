4b757c61bc92303f38d7343ac7fb5d099048ad2d
David Da Silva De Almeida <davda-si@c2r7s4.42lisboa.com>
2024-08-29 20:42:35 +0100

    just need to find player and check around chrs

--- src/map_parse.c
+++ src/map_parse.c
@@ -6,7 +6,7 @@
 /*   By: davda-si <davda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 19:36:11 by davda-si          #+#    #+#             */
-/*   Updated: 2024/08/28 19:48:55 by davda-si         ###   ########.fr       */
+/*   Updated: 2024/08/29 20:25:57 by davda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,41 @@ static int	check_each_line(char **map)
 	return (0);
 }
 
-static check_st_end(char **map)
+static int	check_st_end(char **map)
 {
-	
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	while(map[i])
+	{
+		j = 0;
+		k = ft_strlen(map[i]) - 1;
+		while (map[i][j] == ' ' || (map[i][j] >= 9 && map[i][j] <= 13))
+			j++;
+		while (map[i][k] == ' ' || (map[i][k] >= 9 && map[i][k] <= 13))
+			k--;
+		if (map[i][j] != '1' || map[i][k] != '1')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+static int	check_around_chr(char **map)
+{
+	int		i;
+	int		k;
+	char	*letters;
+	char	*in;
+
+	letters = "10NEWS";
+	in = " NEWS";
+	i = 0;
+	while (map[i])
+	{
+		
+	}
 }
 
 int	map_parse(t_mapdata *map)
